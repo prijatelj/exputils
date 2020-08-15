0f9810ee45a303d6935de3e9897210feb3a94e0b
prijatelj <theomnipotentwoof@gmail.com>
2020-08-15 16:24:32 -0400

    removed deletions due to breaking functinality.

--- exputils/__init__.py
+++ exputils/__init__.py
@@ -3,25 +3,7 @@ from exputils import profile
 # TODO decide if kfold is necessary to install here. Perhaps just an example.
 
 
-# Currently this is the best way to blacklist the file's imports, afaik.
-# Basically everything in io is wanted in the namespace except for io's
-# imports.
 
-# blacklist io imports
-del io.argparse
-del io.deepcopy
-del io.datetime
-del io.json
-del io.logging
-del io.os
-del io.sys
-
-# TODO del io.h5py once imported
-del io.np
-
-# blacklist profile imports
-del profile.datetime
-del profile.contextmanager
-del profile.logging
-del profile.time
-del profile.process_time
+# TODO Cannot del from the namespace, otherwise it breaks the code. Need to
+# figure out another way to get exactly desired namespace and to avoid
+# `exputils.io.np` or `exputils.io.json`
