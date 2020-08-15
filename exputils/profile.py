47a42f5b18cb06b2c4411f7a5d2ec28891c37a84
prijatelj <theomnipotentwoof@gmail.com>
2020-08-15 13:00:50 -0400

    added init profile.py

---
+++ exputils/profile.py
@@ -0,0 +1,10 @@
+"""Convenience functions for profiling python code."""
+from datetime import datetime
+
+
+class Time(object):
+    def __init__(self, datetime_fmt):
+
+def time(func, *args, **kwargs):
+    start_time
+    results = func(*args, **kwargs)
