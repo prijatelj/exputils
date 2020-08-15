4e8a29686e451e586ec29a810282e27195407f56
prijatelj <theomnipotentwoof@gmail.com>
2020-08-15 13:00:22 -0400

    made installable, io only atm.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+long_description = ''
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+install_requires = ''
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read()
+
+setup(
+    name='exputils',
+    version='0.1.0',
+    author='Derek S. Prijatelj',
+    author_email='dprijate@nd.edu',
+    packages=['exputils'],
+    #scripts
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    install_requires=install_requires,
+    python_requires='>=3.7',
+)
