6ed3b03ddbd91eb09d9fa9ca877b5e6fae17ab47
prijatelj <theomnipotentwoof@gmail.com>
2020-11-02 15:53:34 -0500

    incremented the smallest level cuz changes to code were made.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='exputils',
-    version='0.1.0',
+    version='0.1.1',
     author='Derek S. Prijatelj',
     author_email='dprijate@nd.edu',
     packages=[
