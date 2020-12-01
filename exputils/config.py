ac9487e6f37b362d94ba1fc95098c159c116a821
prijatelj <theomnipotentwoof@gmail.com>
2020-11-30 19:40:22 -0500

    initial commit

---
+++ exputils/config.py
@@ -0,0 +1,12 @@
+"""Create argparse compatible configuration objects of objects in Python as
+determined by their docstring.
+"""
+import argparse
+
+# TODO create the dectorator @config that wraps any function or class, takes
+# the docstring, optionally checks if docstring roughly matches class to avoid
+# issues mismatching docstring to object, and optionally creates the argparse
+# parser for that object using the information from the docstring.
+
+# TODO check primitives, confirm all primitives and basic numpy dtypes are as
+# stated in docstring
