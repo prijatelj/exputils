24cb935bff0f7703d972590958dfe47fcccfa95f
prijatelj <theomnipotentwoof@gmail.com>
2021-01-09 22:37:05 -0500

    udpated init

--- exputils/data/__init__.py
+++ exputils/data/__init__.py
@@ -1,8 +1,4 @@
-from exputils.data.confusion_matrix import (
-    ConfusionMatrix,
-    load_confusion_mat,
-)
-
+from exputils.data.confusion_matrix import ConfusionMatrix
 from exputils.data import labels
 
-__all__ = ['ConfusionMatrix', 'load_confusion_mat']
+__all__ = ['ConfusionMatrix']
