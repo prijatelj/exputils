a6f08cb4f615315923207364183eb67b55809cec
prijatelj <theomnipotentwoof@gmail.com>
2021-03-16 19:00:00 -0400

    simplified the init file cuz write once makes sense.

--- exputils/__init__.py
+++ exputils/__init__.py
@@ -1,12 +1,5 @@
 __version__ = '0.2.0'
 
-from exputils import io
-from exputils import data
-from exputils import ml
-from exputils import profile
-from exputils import ray
-from exputils import visuals
-
 __all__ = [
     'io',
     'data',
@@ -15,3 +8,5 @@ __all__ = [
     'ray',
     'visuals',
 ]
+
+from exputils import __all__
