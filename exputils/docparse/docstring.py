01595fbcce669e564e57eec7b8563ce20a96d2df
prijatelj <theomnipotentwoof@gmail.com>
2021-03-25 19:56:09 -0400

    WIP: added refinement of data classes and roughing in the parser.

--- exputils/docparse/docstring.py
+++ exputils/docparse/docstring.py
@@ -1,7 +1,10 @@
 """Code for managing docstring parsing and conversion."""
 from enum import Flag, unique
+from collections import OrderedDict
 from dataclasses import dataclass, InitVar
 from keyword import iskeyword
+import re
+from typing import NamedTuple
 
 from sphinx.ext.autodoc import getdoc, prepare_docstring
 from sphinx.ext.napoleon import Config, GoogleDocstring, NumpyDocstring
@@ -35,7 +38,9 @@ class ValueExists(Flag):
     false = False
 
 
-# TODO Docstring object that contains the parts of the docstring post parsing
+# Docstring object that contains the parts of the docstring post parsing
+# TODO For each of these dataclasses, make them tokenizers for their respective
+# parts of the docstring and string call them during parsing
 @dataclass
 class VariableDoc(object):
     """Dataclass for return objects in docstrings."""
@@ -49,10 +54,12 @@ class VariableDoc(object):
         else:
             raise ValueError(f'`name` is an invalid variable name: `{name}`')
 
+
 @dataclass
-class Parameter(VariableDoc):
+class ParameterDoc(VariableDoc):
     """Dataclass for parameters in docstrings."""
     default : object = ValueExists.false
+
 
 @dataclass
 class Docstring(VariableDoc):
@@ -66,12 +73,23 @@ class Docstring(VariableDoc):
         The function's Arguments/Parameters or the class' Attributes.
     """
     short_description : str
-    args : {str : ParameterDoc}
-    return_doc : InitVar[VariableDoc]
-    # TODO general sections: {section_name : str}
+    other_sections: OrderedDict({str : str})
 
-    def __post_init__(self, args, return_doc):
-        raise NotImplementedError()
+
+@dataclass
+class FuncDocstring(Docstring):
+    """Function docstring components.
+
+    Attributes
+    ----------
+    short_description : str
+        The short description of the docstring.
+    args : str
+        The function's Arguments/Parameters or the class' Attributes.
+    """
+    args : OrderedDict({str : ParameterDoc})
+    other_args : OrderedDict({str : ParameterDoc}) # TODO implement parsing
+    return_doc : VariableDoc = ValueExists.false
 
     def get_str(self, style):
         """Returns the docstring as a string in the given style. Could simply
@@ -92,6 +110,16 @@ class Docstring(VariableDoc):
         raise NotImplementedError()
 
 
+@dataclass
+class ClassDocstring(Docstring):
+    """The multiple Docstrings the make up a class, including at least the
+    class' docstring and the __init__ method's docstring.
+    """
+    attributes : OrderedDict({str : ParameterDoc})
+    init_docstring : FuncDocstring
+    methods : {str: FuncDocstring}
+
+
 class DocstringParser(object):
     """Docstring parsing.
 
@@ -99,53 +127,193 @@ class DocstringParser(object):
     ----------
     style : {'rst', 'numpy', 'google'}
         The style expected to parse.
-    doc_linking : bool, default False
+    doc_linking : bool = False
+    config : sphinx.ext.napoleon.Config = None
     """
-    def __init__(self, style, doc_linking=False):
+    def __init__(self, style, doc_linking=False, config=None):
         if (style := style.lower()) not in {'rst', 'numpy', 'google'}:
             raise ValueError(
                 "Expected `style` = 'rst', 'numpy', or 'google', not `{style}`"
             )
+        # TODO allow the passing of a func/callable to transform custom doc
+        # styles
         self.style = style
         self.doc_linking = doc_linking
 
-        self.config = Config(napoleon_use_param=True, napoleon_use_rtype=True)
-        raise NotImplementedError()
+        if config is None:
+            self.config = Config(
+                napoleon_use_param=True,
+                napoleon_use_rtype=True,
+            )
+        else:
+            self.config = config
 
-    def parse(docstring, style=None, doc_linking=False):
-        """
-        Args
-        ----
-        docstring : str
-            docstring to be parsed
-        doc_linking : bool, default False
-            Linked docstring whose content applies to this docstring and will
-            be parsed recursively.
+        self.re_param_or_returns = re.compile(':(?:param|returns)')
 
-            Add whitelisting of which packages to allow recursive doc linking
+        # TODO section tokenizer (then be able to tell if the section is one of
+        #   param/arg or type and to pair those together.
+        #   attribute sections `.. attribute:: attribute_name`
 
-        Returns
-        -------
-        Docstring
-            A Docstring object containing the essential parts of the Docstring.
-        """
-        raise NotImplementedError()
+        self.re_returns = re.compile(':returns: (?P<doc>.*)', re.S)
+        self.re_param = re.compile(
+            ':param: (?P<name>[\*\w]+): (?P<doc>.*?)'
+                + '(?:(?=:param)|(?=:return)|(?=:raises)|\Z)',
+            re.S,
+        )
+
+    def parse_func(docstring, name, obj_type):
+        """Parse the docstring of a function."""
         docstring = prepare_docstring(docstring)
 
-        style = self.style if style is None else style
-        doc_linking = self.doc_linking if doc_linking is None else doc_linking
+        # NOTE for now, use what is specified at initialization
+        #style = self.style if style is None else style.lower()
+        #doc_linking = self.doc_linking if doc_linking is None else doc_linking
 
-        # Ensure the docstring is in reStructuredText styling
+        # Convert the docstring is in reStructuredText styling
         if self.style == 'google':
             docstring = GoogleDocstring(docstring, self.config)
         elif self.style == 'numpy':
             docstring = NumpyDocstring(docstring, self.config)
+        # TODO allow the passing of a func/callable to transform custom doc
+        # styles
 
         # TODO parse the RST docstring
-        #   TODO get short description, long description.
         #   TODO find Parameters/Args and other Param like sections.
         #   TODO Get all param names, their types (default to object w/ logging
         #   of no set type), and their descriptions
 
-        # Return the Docstring Object that stores that docsting ingo
-        return Docstring()
+        # Prepare the paired sets for parameters to catch dups, & missing pairs
+        param_set = set()
+        type_set = set()
+
+        # Short description is always the first line only
+        short_description = docstring[0]
+
+        # TODO Get the start indices of any & all sections (Could parallelize)
+        section_start_indices = []
+        param_indices = []
+        type_indices = []
+        attr_indices = []
+        returns = None
+        rtype = None
+        # TODO this would be way nicer with a kind of top down tokenization
+        # where each token then can be further broken down, so Section, then
+        # the section type, then its parts. This is where the above dataclasses
+        # having their own parser functions for RST would be good. This would
+        # probably involve back tracking tho and multiple passes.
+        for i, line in enumerate(docstring):
+            if self.re_section.match(line)
+                section_start_indices.append(i)
+
+                if self.re_param.match(line):
+                    param_indices.append(i)
+                elif self.re_type.match(line):
+                    param_indices.append(i)
+                elif self.attribute.match(line):
+                    attr_indices.append(i)
+                elif self.re_returns.match(line):
+                    if returns is None:
+                        returns = PARSE_RETURNS_SECTION
+                    else:
+                        raise SyntaxError(' '.join([
+                            'The docstring cannot have more than one',
+                            '`returns` section.',
+                        ]))
+                elif self.re_rtype.match(line):
+                    if rtype is None:
+                        rtype = PARSE_RTYPE_SECTION
+                    else:
+                        raise SyntaxError(' '.join([
+                            'The docstring cannot have more than one `rtype`',
+                            'section.',
+                        ]))
+
+        # Long description initial text till next section or end of str.
+        if len(section_start_indices) < 1:
+            raise ValueError('The given docstring includes no sections.')
+        # TODO replace w/ replacement of all empty str lines w/ \n and then
+        # join w/ spaces
+        long_description = '\n'.join(docstring[:section_start_indices[0]])
+
+        # Rejoin lines into one str for regex. This strikes me as inefficient
+        docstring = '\n'.join(docstring)
+
+        # Find all sections (field lists in RST for python docstrings)
+
+
+        # Return the Docstring Object that stores that docsting info
+        raise NotImplemented()
+    def parse(
+        obj,
+        style=None,
+        doc_linking=False,
+        name=None,
+        obj_type=None,
+        func_list=None
+    ):
+        """
+        Args
+        ----
+        obj : object | str
+            The object whose __doc__ is to be parsed. If a str object of the
+            docstring, then the `name` and `obj_type` parameters must be
+            provided.
+        style : str, default None
+            Expected docstring style determining how to parse the docstring.
+        doc_linking : bool, default False
+            Linked docstring whose content applies to this docstring and will
+            be parsed recursively.
+
+            TODO Add whitelisting of which packages to allow recursive doc
+            linking
+        name : str, optional
+            The name of the object whose docstring is being parsed. Only needs
+            to be supplied when the `obj` is a `str` of the docstring to be
+            parsed, otherwies not used.
+        obj_type : type, optional
+            The type of the object whose docstring is being parsed. Only needs
+            to be supplied when the `obj` is a `str` of the docstring to be
+            parsed, otherwies not used.
+        func_list : [str], optional
+            A list of method names whose docstrings are all to be parsed and
+            returned in a hierarchical manner encapsulating the hierarchical
+            docstring of a class.  Only used when `obj` is a class object.
+
+        Returns
+        -------
+        FuncDocstring | ClassDocstring
+            A Docstring object containing the essential parts of the Docstring,
+            or a ClassDocstring which contains the Docstring objects of the
+            methods of the class along with the class' parsed Docstring object.
+        """
+        # TODO optionally parallelize the parsing, perhaps with Ray?
+        if isinstance(obj, str):
+            if name is None or obj_type is None:
+                raise ValueError(' '.join([
+                    'If `obj` is a `str` then must also give `name` and',
+                    '`obj_type`',
+                ]))
+            return self.parse_func(obj, name, obj_type)
+        elif hasattr(obj, '__doc__') and hasattr(obj, '__name__'):
+            # Obtain info from the object itself
+            name = obj.__name__
+            obj_type = type(obj)
+
+            # TODO if a class, then parse the __init__ too, but as the main
+            # docstring of interest. As it defines the params to give, and thus
+            # the args we care about.
+            if isinstance(obj_type, type):
+                class_docstring = obj.__doc__
+                init_docstring = self.parse_func(obj.__init__)
+
+                raise NotImplementedError('Need to add parsing of a class')
+                # TODO add parsing of a class' methods in given list.
+                return ClassDocstring(name, obj_type, description
+
+            return self.parse_func(obj.__doc__, name, obj_type)
+        else:
+            raise TypeError(' '.join([
+                'Expected `obj` to be an object with `__doc__` and `__name__`',
+                'attributes, or a `str` with the `name` and `obj_type`',
+                'parameters given.',
+            ]))
