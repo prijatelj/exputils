5d7d7e6fe4e3d5b4e5feb9d89a4270bd93128b3e
prijatelj <theomnipotentwoof@gmail.com>
2022-02-09 20:15:59 -0500

    added scatter nd in numpy and copied working result from ipython

--- exputils/data/confusion_tensor.py
+++ exputils/data/confusion_tensor.py
@@ -3,9 +3,24 @@ import numpy as np
 
 from exputils.data.labels import NominalDataEncoder as NDE
 
+def scatter_nd_numpy(indices, updates, shape, target=None):
+    """Equivalent to tf.scatter_nd()."""
+    # TODO match tf scatter_nd interface
+    # This does not work for recurring indices, which is necessary for CMs
+    if target is None:
+        target = np.zeros(shape, dtype=updates.dtype)
+    np.add.at(
+        target,
+        tuple(indices.reshape(-1, indices.shape[-1]).T),
+        updates.ravel(),
+    )
+    if target is None:
+        return target
+
+
 class OrderedConfusionTensor(ConfusionMatrix):
-    """A Tensor of KxCxC where K dimension represents the ordered confusion,
-    and max(K) == C with each individual CxC along the K dimenison is the
+    """A Tensor of KxCxC where K axisension represents the ordered confusion,
+    and max(K) == C with each individual CxC along the K axisenison is the
     ordered slice of the ordered predictions. Looking at one slice of the
     internal confusion tensor is NOT the top-k confusion matrix, but may be
     used to obtain the top-k measures.
@@ -16,8 +31,10 @@ class OrderedConfusionTensor(ConfusionMatrix):
         preds,
         labels,
         top_k=None,
-        dim=1,
+        axis=1,
         sort_labels=False,
+        targets_idx=True,
+        weights=None,
     ):
         """Calculates the top-k confusion matrix of occurrence from the pairs.
 
@@ -33,24 +50,51 @@ class OrderedConfusionTensor(ConfusionMatrix):
         # NominalDataEncoder of labels
         self.label_enc = NDE(labels)
 
-        assert(pred.shape[dim] == len(labels))
+        assert(preds.shape[axis] == len(labels))
+
+        n_classes = len(self.label_enc)
 
         if top_k is None:
-            top_k = len(self.label_enc)
+            top_k = n_classes
         else:
             assert(isinstance(top_k, int))
 
         # Get top-k predictions, assuming prediction
-        top_preds = np.argsort(preds, dim)[:, ::-1][:, :top_k]
-
-        # TODO Boolean check if target in top_preds: Only good for binary CM
+        top_preds = np.argsort(preds, axis)[:, ::-1][:, :top_k]
 
         # TODO Create a confusion tensor of ordered confusion? The order is
         # informtive, and a confusion matrix/tensor or similar construct may
         # capture this information.
 
-        # TODO cast both targets and preds to their args for a confusion matrix
+        # Cast both targets and preds to their args for a confusion matrix
         # based on label_enc
+        if not targets_idx:
+            targets = self.label_enc.encode(targets)
 
+        # TODO Repeat targets to align w/ top_preds with the K index, then
+        # scatter_nd would perform the desired k-th ordered confusion matrix
+        # creation.
 
-        self.tensor = ...
+        # TODO Repeat this top_k times
+        ordered_cms = []
+        for k in range(top_k):
+            unique_idx, unique_idx_counts = np.unique(
+                np.stack([targets, top_preds[:, [k]]], axis=axis),
+                return_counts=True,
+            )
+
+            if weights is not None:
+                unique_idx_n *= weights
+
+            ordered_cms.append(
+                scatter_nd_numpy(
+                    unique_idx.T,
+                    updates=unique_idx_counts,
+                    shape=(n_classes, n_classes),
+                )
+            )
+
+        self.tensor = np.stack(ordered_cms)
+
+    # TODO obtain BinaryTopKConfusionMatrix / Tensor and its associated
+    # measures.
