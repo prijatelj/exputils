c96b9077c83ad63ab47f2f6df290ebd4734f5fa8
prijatelj <theomnipotentwoof@gmail.com>
2022-04-30 15:52:49 -0400

    rm ray and visuals as empty, updated working version number

--- exputils/__init__.py
+++ exputils/__init__.py
@@ -1,14 +1,15 @@
 from importlib import import_module
-__version__ = '0.1.6'
+__version__ = '0.1.7rc0'
 
 __all__ = [
     'io',
     'data',
     'ml',
     'profile',
-    'ray',
-    'visuals',
+    #'ray',
+    #'visuals',
 ]
 
 for module in __all__:
     globals()[module] = import_module(f'.{module}', 'exputils')
+del import_module, module
