39da6627fc3e60f4b75af913e24bdc65ae96d225
prijatelj <theomnipotentwoof@gmail.com>
2022-04-30 19:05:40 -0400

    changed to ordered ... atrices

--- exputils/data/__init__.py
+++ exputils/data/__init__.py
@@ -1,5 +1,5 @@
 from exputils.data.confusion_matrix import ConfusionMatrix
-from exputils.data.confusion_tensor import OrderedConfusionTensor
+from exputils.data.ordered_confusion_matrix import OrderedConfusionMatrices
 from exputils.data import labels
 
-__all__ = ['ConfusionMatrix', 'OrderedConfusionTensor']
+__all__ = ['ConfusionMatrix', 'OrderedConfusionMatrices']
