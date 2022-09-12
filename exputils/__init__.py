08a4abf0987c35447683419ca85e0d09cedd6e15
Derek S. Prijatelj <derek.s.prijatelj@gmail.com>
2022-09-12 16:07:39 +0000

    Added unit tests for NominalDataEncoder

--- exputils/__init__.py
+++ exputils/__init__.py
@@ -1,14 +1,15 @@
 from importlib import import_module
-__version__ = '0.1.5'
+__version__ = '0.1.7rc3'
 
 __all__ = [
     'io',
     'data',
     'ml',
     'profile',
-    'ray',
-    'visuals',
+    #'ray',
+    #'visuals',
 ]
 
 for module in __all__:
     globals()[module] = import_module(f'.{module}', 'exputils')
+del import_module, module
